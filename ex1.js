f7bb33c7e47061f4ffc267f864fc035ce2a3cc3f
Milena-Div <168042821+Milena-Div@users.noreply.github.com>
2024-05-17 22:35:42 -0300

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,24 @@
+
+
+//exercício 1
+
+const numero = 25
+
+if(numero>10)
+{console.log("esse número é maior que 10")}
+else {console.log("esse número é menor que 10")}
+
+//exercício 2
+
+const numero1 = 1
+const numero2 = 99
+
+if(numero1 + numero2 > 100){
+    console.log("Esse número é maior que 100")}
+
+else if (numero1+numero2===100) {
+    console.log("Bingo! Deu 100!")}
+
+else{console.log("Esse número é menor que 100")} 
+
+
